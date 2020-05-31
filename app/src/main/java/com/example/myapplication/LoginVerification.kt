bd8a54696f1ac7756bd47b0ed6c3ae5b7ad23b51
Craig Brazelton <brazeschool@gmail.com>
2020-05-30 23:16:36 -0700

    change method name 2

--- app/src/main/java/com/example/myapplication/LoginVerification.kt
+++ app/src/main/java/com/example/myapplication/LoginVerification.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication
 
 class LoginVerification {
-    fun verify(email: String, password: String): Boolean {
+    fun verifyEmailAndPAssword(email: String, password: String): Boolean {
         return email == "test@craig.com" &&
                 password == "testing"
     }
